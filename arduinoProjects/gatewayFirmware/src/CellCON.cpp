9008ecf160a8e979705a36a53f55c33454166d0c
StayFrostea <59980526+StayFrostea@users.noreply.github.com>
2022-01-06 16:46:48 -0700

    Nothing really. Just done for the day.

--- arduinoProjects/gatewayFirmware/src/CellCON.cpp
+++ arduinoProjects/gatewayFirmware/src/CellCON.cpp
@@ -1,12 +1,5 @@
 #include "CellCON.h"
 
-const char APN[] = "Internet.be";
-const char URL[] = "https://postman-echo.com/post";
-const char CONTENT_TYPE[] = "application/json";
-// *** This is what we need to change ***
-// **************************************
-char PAYLOAD[];
-
 CellCON::CellCON(){
 
 }
--- arduinoProjects/gatewayFirmware/src/CellCON.h
+++ arduinoProjects/gatewayFirmware/src/CellCON.h
@@ -6,6 +6,13 @@
 
 #define SIM800_RST_PIN 6
 
+const char APN[] = "Internet.be";
+const char URL[] = "https://postman-echo.com/post";
+const char CONTENT_TYPE[] = "application/json";
+// *** This is what we need to change ***
+// **************************************
+char PAYLOAD[];
+
 SIM800L* sim800l;
 
 class CellCON {
